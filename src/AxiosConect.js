4b5c3ab9d2d2c433343f4ed42801c46666094d6e
SebastianCastro <43289534+jagudelocastro19@users.noreply.github.com>
2022-11-18 19:58:41 -0500

    Nuevos cambios

--- src/AxiosConect.js
+++ src/AxiosConect.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const BASE_URL = 'https://animalariumshop.herokuapp.com/'; conexion en despliegue
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3001';
 
 const apiInstance = axios.create({baseURL:BASE_URL});
 
